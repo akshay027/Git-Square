95c40cbccb7fbcaa364874a5ad461800dcf37ab2
akshay027 <mahurkarakshay242@gmail.com>
2018-01-22 12:47:46 +0530

    Git Square Code

--- app/src/main/java/com/example/exalogic/gitsquare/SquareContribsActivity.java
+++ app/src/main/java/com/example/exalogic/gitsquare/SquareContribsActivity.java
@@ -1,14 +1,15 @@
 package com.example.exalogic.gitsquare;
 
+
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -42,11 +43,15 @@ public class SquareContribsActivity extends AppCompatActivity {
     private static final String JSON_URL = "https://api.github.com/repos/square/retrofit/contributors";
     private Button filterBtn;
     private SwipeRefreshLayout swipeRefreshLayout;
+     ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Square Contribs");
 
         square_list_item_view = (RecyclerView) findViewById(R.id.list_item);
         filterBtn = (Button) findViewById(R.id.filterBtn);
